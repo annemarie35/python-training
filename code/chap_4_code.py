7ff671c0fde26ddf38a936758281662b3d5c456e
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-09-02 10:49:22 +0200

    [chapter 4]: arguments, test, functions

--- code/chap_4_code.py
+++ code/chap_4_code.py
@@ -35,3 +35,47 @@ powers = (1, 2, 3)
 epsilons = (0.1, 0.001, 1)
 
 test_find_root(x_vals, powers, epsilons)
+
+
+def mean(*args):
+    # Assumes at least one argument and all arguments are numbers
+    # Returns the mean of the arguments
+    total = 0
+    for arg in args:
+        total += arg
+    return total / len(args)
+
+
+mean(1, 2, 3, 4)
+
+def f(x): #name x used as formal parameter
+    y = 1
+    x = x + y
+    print('x =', x)
+    return x
+x = 3
+y = 2
+z = f(x) #value of x used as actual parameter
+print('z =', z)
+print('x =', x)
+print('y =', y)
+
+def nested_scopes(x):
+    def func_g():
+        x = 'abc'
+        print('x =', x)
+    def func_h():
+        z = x
+        print('z =', z)
+    x = x + 1
+    print('x =', x)
+    func_h()
+    func_g()
+    print('x =', x)
+    return func_g
+
+x = 3
+z = nested_scopes(x)
+print('x =', x)
+print('z =', z)
+z()
